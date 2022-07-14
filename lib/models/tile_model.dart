6ba5bf3ed7932773ef09ab95d497d587c4ee8f62
Bayu Wicaksono <57973246+xinz8420@users.noreply.github.com>
2022-07-14 22:09:41 +0700

    Add files via upload

---
+++ lib/models/tile_model.dart
@@ -0,0 +1,10 @@
+import 'package:guess_the_word/constants/answer_stages.dart';
+
+class TileModel {
+
+  final String letter;
+  AnswerStage answerStage;
+
+  TileModel({required this.letter, required this.answerStage});
+
+}
